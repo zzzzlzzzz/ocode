9c7cd910b18f9c3b66893f89fca3fd8f19d17c36
zzzzlzzzz <zzzzlzzzz@yandex.com>
2019-10-19 17:34:08 +0400

    Use argparse FileType Fix numeration

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from collections import Counter
 from math import ceil
-from argparse import ArgumentParser, Namespace
+from argparse import ArgumentParser, Namespace, FileType
 
 
 import numpy as np
@@ -14,7 +14,14 @@ def generate(args: 'Namespace') -> None:
     :param args: CL-arguments
     :return: None
     """
-    np.savetxt(args.destination, (np.random.random([args.m, args.n]) * 2.0) - 1.0, delimiter=',')
+    if args.m <= 0:
+        print('M must be greater than zero')
+        return
+    if args.n <= 0:
+        print('N must be greater than zero')
+        return
+    with args.destination:
+        np.savetxt(args.destination, (np.random.random([args.m, args.n]) * 2.0) - 1.0, delimiter=',')
 
 
 def calculate(args) -> None:
@@ -27,14 +34,18 @@ def calculate(args) -> None:
     max_distance, max_i, max_j = None, None, None
     hist = Counter()
 
-    array = np.loadtxt(args.source, delimiter=',')
-    for i, row_a in enumerate(array[: len(array) - 1]):
-        for j, row_b in enumerate(array[i + 1:]):
+    with args.source:
+        matrix = np.loadtxt(args.source, delimiter=',')
+    if len(matrix) < 2:
+        print('Matrix must consist 2 or more rows')
+        return
+    for i, row_a in enumerate(matrix[: len(matrix) - 1]):
+        for j, row_b in enumerate(matrix[i + 1:]):
             distance = np.linalg.norm(row_a - row_b)
             if min_distance is None or distance < min_distance:
-                min_distance, min_i, min_j = distance, i, j
+                min_distance, min_i, min_j = distance, i, j + i + 1
             if max_distance is None or distance > max_distance:
-                max_distance, max_i, max_j = distance, i, j
+                max_distance, max_i, max_j = distance, i, j + i + 1
             hist[ceil(distance * 10) / 10] += 1
 
     labels, values = zip(*sorted(hist.items(), key=lambda _: _[0]))
@@ -55,13 +66,13 @@ def main() -> None:
     subparsers.metavar = 'ACTION'
 
     parser_generate = subparsers.add_parser('gen', help='Generate matrix')
-    parser_generate.add_argument('destination', help='Destination file', metavar='DST')
+    parser_generate.add_argument('destination', type=FileType('w'), help='Destination file', metavar='DST')
     parser_generate.add_argument('m', type=int, help='Matrix rows', metavar='M')
     parser_generate.add_argument('n', type=int, help='Matrix cols', metavar='N')
     parser_generate.set_defaults(func=generate)
 
     parser_calculate = subparsers.add_parser('calc', help='Calculate matrix')
-    parser_calculate.add_argument('source', help='Source file', metavar='SRC')
+    parser_calculate.add_argument('source', type=FileType('r'), help='Source file', metavar='SRC')
     parser_calculate.add_argument('destination', help='Destination image file (e.g. r.png)', metavar='DST')
     parser_calculate.set_defaults(func=calculate)
 
